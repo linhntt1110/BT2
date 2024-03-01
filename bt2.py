56abda0006b8930998d8bfbd06b0da4dc59eec73
linhntt1110 <161816092+linhntt1110@users.noreply.github.com>
2024-03-01 16:16:18 +0700

    Update bt2.py

--- bt2.py
+++ bt2.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 # Data Set
-data = pd.read_csv("./files/avocado.csv")
+data = pd.read_csv("avocado_full.csv")
 # Minimizing Dataset
 albany_df = data[data['region']=="Albany"]
 al_df = albany_df[albany_df["year"]==2015]
-# Horizontal Bar Graph
+#Bar
 bar_graph = px.bar(
 al_df,
 x = "Large Bags",
@@ -15,3 +15,5 @@ title = "Bar Graph",
 color="Large Bags",
 orientation='h'
 )
+st.header("Bar Chart")
+st.plotly_chart(bar_graph)
