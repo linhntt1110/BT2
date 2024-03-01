256a167f76ec3692536e62d8c41b7826c63cf256
linhntt1110 <161816092+linhntt1110@users.noreply.github.com>
2024-03-01 15:57:26 +0700

    Add files via upload

---
+++ bt2.py
@@ -0,0 +1,17 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+# Data Set
+data = pd.read_csv("avocado_full.csv")
+# Minimizing Dataset
+albany_df = data[data['region']=="Albany"]
+al_df = albany_df[albany_df["year"]==2015]
+#Bar
+bar_graph = px.bar(
+x = al_df["Date"],
+y = al_df["Large Bags"],
+title = "Bar Graph",
+color=al_df["Large Bags"]
+)
+st.header("Bar Chart")
+st.plotly_chart(bar_graph_chart)
